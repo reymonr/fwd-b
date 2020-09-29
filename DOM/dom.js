8b0025cc197df9770d726a1c9991192682f96d87
unknown <reymonr@unklab.ac.id>
2020-09-29 09:59:29 +0800

    javascript dom example

---
+++ DOM/dom.js
@@ -0,0 +1,43 @@
+/**
+ * SELECTORS
+ *
+ * getElementById() - single
+ * getElementsByClassName() - multiple
+ * getElementsByTagName() - multiple
+ *
+ * querySelector() - single
+ * querySelectorAll() - multiple
+ */
+
+// getElementById()
+// const headerTitle = document.getElementById('header-title');
+// console.log(headerTitle);
+// console.log(headerTitle.textContent);
+// headerTitle.textContent = 'Student App';
+// headerTitle.innerText = 'FWD A Student List';
+// headerTitle.innerHTML = '<h5>FWD-A Students</h5>';
+// headerTitle.style.color = 'red';
+// headerTitle.style.backgroundColor = '#fff';
+// headerTitle.style.fontWeight = 'bold';
+
+// getElementsByClassName()
+// const students = document.getElementsByClassName('list-group-item');
+// console.log(students);
+// console.log(students[1].textContent);
+// students[0].textContent = '105021810090 / Ken Thompson';
+// students[1].style.fontWeight = 'bold';
+// students[2].style.textDecoration = 'line-through';
+// students[3].style.backgroundColor = '#bbb';
+
+// for (st of students) {
+// 	st.style.backgroundColor = '#a1b1c1';
+// }
+
+// getElementsByTagName()
+// const students = document.getElementsByTagName('li');
+// console.log(students);
+// console.log(students[0].textContent);
+
+// for (st of students) {
+// 	st.style.backgroundColor = '#a1b1c1';
+// }
